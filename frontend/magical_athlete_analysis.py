bd6d210a2b8cc19de7b08405e3189dea104fd439
Philipp Schoneville <philipp@schoneville.com>
2026-02-23 23:05:25 +0100

    chore(release): prepare v1.0.0

--- frontend/magical_athlete_analysis.py
+++ frontend/magical_athlete_analysis.py
@@ -45,7 +45,7 @@ async def cell_import():
     from rich.console import Console
     from rich.logging import RichHandler
 
-    MAGSIM_VERSION = "0.7.1"
+    MAGSIM_VERSION = "1.0.0"
     await micropip.install(
         f"magsim=={MAGSIM_VERSION}",
         keep_going=True,
